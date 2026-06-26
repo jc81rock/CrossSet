c3a38d2e698ba3dbff5eafb5490af9173daffb9d
Jefferson de Castro Lima <74632090+jc81rock@users.noreply.github.com>
2026-06-26 14:14:02 -0300

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@
 const SUPABASE_URL = "https://hfpvbsoaszenrfjzbbjt.supabase.co";
 const SUPABASE_KEY = "sb_publishable_KUlqpOD-RqTCtt16TvwA8g_jLtYDsU_";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
